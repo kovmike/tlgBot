8f5f4eddde6fc5b73ac7415ebd464e754f505716
kovmike <>
2020-05-12 12:53:52 +0300

    1
    1

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Telegraf = require("telegraf");
+const Extra = require("telegraf/extra");
+const Markup = require("telegraf/markup");
 
 const bot = new Telegraf("1240633700:AAFMcBpugVrQUiXVQY-jlm6iwcDwrOIl0u0");
 bot.use(Telegraf.log());
@@ -7,5 +9,8 @@ bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.command("echo", (ctx) => ctx.reply("Echo"));
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-console.log(bot);
+bot.command("simple", (ctx) => {
+  return ctx.replyWithHTML("<b>Coke</b> or <i>Pepsi?</i>", Extra.markup(Markup.keyboard(["Coke", "Pepsi"])));
+});
+
 bot.launch();
